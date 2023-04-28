84a4c4741f71fe8f6fcfb15ffd47ee5c2f2bba69
Olumide Michael <olumide.m.ojo@gmail.com>
2023-04-28 15:16:46 +0100

    fix: modified image path

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -4,10 +4,10 @@ import '../assets/style/socialMediaIcon.css'
 const SocialMediaIcon = () => {
   return (
     <span className='social-media-icon-wrapper'>
-        <a href='' target='_blank'><img className='social-media-icon' src='src/assets/logo/facebookIcon.png'/> <span className='name'>Facebook</span></a>
-        <a href='' target='_blank'><img className='social-media-icon' src='src/assets/logo/instagramIcon.png' /> <span className='name'>Instagram</span></a>
-        <a href='' target='_blank'><img className='social-media-icon' src='src/assets/logo/linkedinIcon.png' /> <span className='name'>LinkedIn</span></a>
-        <a href='' target='_blank'><img className='social-media-icon' src='src/assets/logo/twitterIcon.png' /><span className='name'>Twitter</span></a>
+        <a href='' target='_blank'><img className='social-media-icon' src='logo/facebookIcon.png'/> <span className='name'>Facebook</span></a>
+        <a href='' target='_blank'><img className='social-media-icon' src='logo/instagramIcon.png' /> <span className='name'>Instagram</span></a>
+        <a href='' target='_blank'><img className='social-media-icon' src='logo/linkedinIcon.png' /> <span className='name'>LinkedIn</span></a>
+        <a href='' target='_blank'><img className='social-media-icon' src='logo/twitterIcon.png' /><span className='name'>Twitter</span></a>
     </span>
   )
 }
