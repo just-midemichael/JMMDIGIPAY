821b1b9552cc05e3046abdc525c61da5e2c08bb6
Olumide Michael <olumide.m.ojo@gmail.com>
2023-05-03 11:46:38 +0100

    fix: pos page content layout

--- src/pages/businessPages/POS.tsx
+++ src/pages/businessPages/POS.tsx
@@ -45,12 +45,6 @@ const POS = () => {
       <section className='section-2 features-section'>
         
           <article className='features-content-container'>
-            <div className='image-focal-point aos aos-fade'>
-              <div className='image-container'>
-                <img className='image ' src='image/pos-image-6.jpg'/>
-              </div>
-            </div>
-
             <div className='text-focal-point aos aos-right'>
               <div className='title '>Move your business forward with DigiPay POS</div>
 
@@ -58,9 +52,21 @@ const POS = () => {
                 Business can find you anywhere. DigiPay POS from helps you receive payments on the move with any smartphone connected to the internet.
               </div>
             </div>
+
+            <div className='image-focal-point aos aos-fade'>
+              <div className='image-container'>
+                <img className='image ' src='image/pos-image-6.jpg'/>
+              </div>
+            </div>
           </article>
 
           <article className='features-content-container'>
+          <div className='image-focal-point aos aos-fade'>
+              <div className='image-container'>
+                <img className='image ' src='image/pos-image-3.jpg'/>
+              </div>  
+            </div>
+
             <div className='text-focal-point aos aos-left'>
               <div className='title '>Keep an eye on your revenue</div>
 
@@ -68,21 +74,9 @@ const POS = () => {
                 You can track all POS transactions in real-time on your DigiPay Business mobile app for Android and iOS.
               </div>
             </div>
-
-            <div className='image-focal-point aos aos-fade'>
-              <div className='image-container'>
-                <img className='image ' src='image/pos-image-3.jpg'/>
-              </div>  
-            </div>
           </article>
 
           <article className='features-content-container'>
-            <div className='image-focal-point aos aos-fade'>
-              <div className='image-container'>
-                <img className='image ' src='image/pos-image-2.jpg'/>
-              </div>
-            </div>
-
             <div className='text-focal-point aos aos-right'>
               <div className='title'>Give your customers more flexible payment option</div>
 
@@ -90,9 +84,21 @@ const POS = () => {
                 DigiPay POS gives you several payment options including transfers, Pay with QR codes, USSD and cards.
               </div>
             </div>
+
+            <div className='image-focal-point aos aos-fade'>
+              <div className='image-container'>
+                <img className='image ' src='image/pos-image-2.jpg'/>
+              </div>
+            </div>
           </article>
 
           <article className='features-content-container'>
+          <div className='image-focal-point aos aos-fade'>
+              <div className='image-container'>
+                <img className='image ' src='image/pos-image-4.jpg'/>
+              </div>  
+            </div>
+
             <div className='text-focal-point aos aos-left'>
               <div className='title '>Supercharge your sales with Sales Mode</div>
 
@@ -100,26 +106,20 @@ const POS = () => {
                 Assign devices with DigiPay POS to your staff and track sales easily wherever you are.
               </div>
             </div>
-
-            <div className='image-focal-point aos aos-fade'>
-              <div className='image-container'>
-                <img className='image ' src='image/pos-image-4.jpg'/>
-              </div>  
-            </div>
           </article>
 
           <article className='features-content-container'>
-            <div className='image-focal-point aos aos-fade'>
-              <div className='image-container'>
-                <img className='image ' src='image/pos-image-1.jpg'/>
-              </div>
-            </div>
-
             <div className='text-focal-point aos aos-right'>
               <div className='title'>Receive payments on the move with DigiPay POS</div>
 
               <div className='text-description '>
                 Open a DigiPay Business account and turn your phone into a POS.
+              </div>
+            </div>
+
+            <div className='image-focal-point aos aos-fade'>
+              <div className='image-container'>
+                <img className='image ' src='image/pos-image-1.jpg'/>
               </div>
             </div>
           </article>
