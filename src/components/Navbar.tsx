1f049192f0225f0ce989b1ad818d911432884f8c
Olumide Michael <olumide.m.ojo@gmail.com>
2023-05-07 00:13:13 +0100

    fix: modified icon and logo path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,19 +18,19 @@ const Navbar = () => {
             <li className='nav-link'>Personal
                 <div className='nav-sub-link-container'>
                     <NavLink to='/budget' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/budget'}>
-                        <img className='sub-link-icon' src='icon/budget-icon.png'/>Budget
+                        <img className='sub-link-icon' src='/icon/budget-icon.png'/>Budget
                     </NavLink>
                     <NavLink to='/investment' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/investment'}>
-                        <img className='sub-link-icon' src='icon/investment-icon.png'/>Investments
+                        <img className='sub-link-icon' src='/icon/investment-icon.png'/>Investments
                     </NavLink>
                     <NavLink to='/saving' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/saving'}>
-                        <img className='sub-link-icon' src='icon/saving-icon.png'/>Savings
+                        <img className='sub-link-icon' src='/icon/saving-icon.png'/>Savings
                     </NavLink>
                     <NavLink to='/loan' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/loan'}>
-                        <img className='sub-link-icon' src='icon/loan-icon.png'/>Loans
+                        <img className='sub-link-icon' src='/icon/loan-icon.png'/>Loans
                     </NavLink>
                     <NavLink to='/card' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/card'}>
-                        <img className='sub-link-icon' src='icon/card-icon.png'/>Cards
+                        <img className='sub-link-icon' src='/icon/card-icon.png'/>Cards
                     </NavLink>
 
                     <div className='drop-diamond'></div>
@@ -39,13 +39,13 @@ const Navbar = () => {
             <li className='nav-link'>Business
                 <div className='nav-sub-link-container'>
                     <NavLink to='/business-registration' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/business-registration'}>
-                        <img className='sub-link-icon' src='icon/business-registration-icon.png'/>Business Registration
+                        <img className='sub-link-icon' src='/icon/business-registration-icon.png'/>Business Registration
                     </NavLink>
                     <NavLink to='/agent' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/agent'}>
-                        <img className='sub-link-icon' src='icon/agent-icon.png'/>Agents
+                        <img className='sub-link-icon' src='/icon/agent-icon.png'/>Agents
                     </NavLink>
                     <NavLink to='/pos' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/pos'}>
-                        <img className='sub-link-icon' src='icon/pos-icon.png'/>POS
+                        <img className='sub-link-icon' src='/icon/pos-icon.png'/>POS
                     </NavLink>
 
                     <div className='drop-diamond'></div>
@@ -54,16 +54,16 @@ const Navbar = () => {
             <li className='nav-link'>Company
                 <div className='nav-sub-link-container'>
                     <NavLink to='/about' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/about'}>
-                        <img className='sub-link-icon' src='icon/about-icon.png'/>About Us
+                        <img className='sub-link-icon' src='/icon/about-icon.png'/>About Us
                     </NavLink>
                     <NavLink to='/blog' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/blog'}>
-                        <img className='sub-link-icon' src='icon/blog-icon.png'/>Blog
+                        <img className='sub-link-icon' src='/icon/blog-icon.png'/>Blog
                     </NavLink>
                     <NavLink to='/career' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/career'}>
-                        <img className='sub-link-icon' src='icon/career-icon.png'/>Career
+                        <img className='sub-link-icon' src='/icon/career-icon.png'/>Career
                     </NavLink>
                     <NavLink to='/contact' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/contact'}>
-                        <img className='sub-link-icon' src='icon/contact-icon.png'/>Contact
+                        <img className='sub-link-icon' src='/icon/contact-icon.png'/>Contact
                     </NavLink>
 
                     <div className='drop-diamond'></div>
@@ -72,16 +72,16 @@ const Navbar = () => {
             <li className='nav-link'>Help
                 <div className='nav-sub-link-container'>
                     <NavLink to='/help' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/help'}>
-                        <img className='sub-link-icon' src='icon/help-icon.png'/>Get Help
+                        <img className='sub-link-icon' src='/icon/help-icon.png'/>Get Help
                     </NavLink>
                     <NavLink to='/scam-awareness' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/scam-awareness'}>
-                        <img className='sub-link-icon' src='icon/scam-icon.png'/>Scam Awareness
+                        <img className='sub-link-icon' src='/icon/scam-icon.png'/>Scam Awareness
                     </NavLink>
                     <NavLink to='/faqs' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/faqs'}>
-                        <img className='sub-link-icon' src='icon/faq-icon.png'/>FAQs
+                        <img className='sub-link-icon' src='/icon/faq-icon.png'/>FAQs
                     </NavLink>
                     <NavLink to='/report-account' className='nav-sub-link' style={activeStyle} onClick={() => window.location.href='/report-account'}>
-                        <img className='sub-link-icon' src='icon/report-account-icon.png'/>Report Accout
+                        <img className='sub-link-icon' src='/icon/report-account-icon.png'/>Report Accout
                     </NavLink>
 
                     <div className='drop-diamond'></div>
