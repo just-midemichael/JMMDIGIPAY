587165ad1f5a1114f92e9774a1028953cf249984
Olumide Michael <olumide.m.ojo@gmail.com>
2023-05-06 23:52:46 +0100

    build: added content

--- src/pages/helpPages/GetHelp.tsx
+++ src/pages/helpPages/GetHelp.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SearchInput from '../../components/input/SearchInput'
+import { NavLink, Outlet } from 'react-router-dom'
+import '../../assets/style/getHelp.css'
 
 const GetHelp = () => {
     const docTitle = () => {
@@ -7,9 +9,26 @@ const GetHelp = () => {
     }
     docTitle()
 
+    const activeStyle = ({isActive}: {isActive: boolean}) => {
+        return {
+            color: isActive? "navy" : "",
+            textDecoration: isActive? "2px underline navy" : "",
+        }
+    }
+
     return (
         <div className='body get-help-body'>
-            <SearchInput/>
+            <div className='search-nav-container'>
+                <div className='search-container'>
+                    <SearchInput/>
+                </div>
+                <nav className='nav-container'>
+                    <NavLink to='/help/home' style={activeStyle}onClick={() => window.location.href='/help/home'}>Home</NavLink>
+                    <NavLink to='/help/ticket' style={activeStyle}onClick={() => window.location.href='/help/ticket'}>Ticket</NavLink>
+                </nav>
+            </div>
+            
+            <section><Outlet/></section>
         </div>
     )
 }
